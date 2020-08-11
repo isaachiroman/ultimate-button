8584d1257ec18f869fa321310b8b667801b88040
isaachiroman <isaachiroman@gmail.com>
2020-08-11 17:05:33 +0700

    fbc

--- asset/js/index.js
+++ asset/js/index.js
@@ -40,7 +40,6 @@ jQuery(document).ready(function($){
       $(".mbwph-main-contact").addClass("isHideElement");
       $(".mbwph-call-main").addClass("isHideElement");
       localStorage.setItem('MBWPH_FBC', isOpen);
-      alert(localStorage.getItem('MBWPH_FBC'));
     }
 
   });
